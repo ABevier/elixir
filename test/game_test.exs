8e7c27e56065754a1273389fa8efc0f749dcc505
Alan Bevier <awbevier@gmail.com>
2021-10-01 21:11:05 -0400

    add a test

--- test/game_test.exs
+++ test/game_test.exs
@@ -2,7 +2,7 @@ defmodule GameTest do
   use ExUnit.Case
   doctest Game
 
-  test "greets the world" do
-    assert Game.hello() == :world
-  end
+  # test "greets the world" do
+  #   assert Game.hello() == :world
+  # end
 end
---
+++ test/position_test.exs
@@ -0,0 +1,19 @@
+defmodule PositionTest do
+  import Position
+  use ExUnit.Case
+  doctest Game
+
+  test "should find range 2 in the middle" do
+    expected = MapSet.new([
+      pos(4, 2),
+      pos(3, 2), pos(2, 2),
+      pos(5, 2), pos(6, 2),
+      pos(4, 1), pos(4, 0),
+      pos(4, 3), pos(4, 4),
+      pos(3, 1), pos(5, 3), pos(5, 1), pos(3, 3)
+    ])
+
+    assert find_all_in_range(pos(4, 2), 2, 9, 5) == expected
+  end
+
+end
